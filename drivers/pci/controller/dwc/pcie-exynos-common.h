05e6befaeeab262a1ccaa62108139501cab24e2e
Tarun Saha <tarun.saha@samsung.com>
2023-08-10 17:22:13 -0700

    exynos-pcie: Register panic notifier callback to dump RC phy SFRs.
    Bug: 291708247
    Change-Id: I9757c2abb54bdce243ffe822955337704f50762c
    Signed-off-by: Tarun Saha <tarun.saha@samsung.com>
    Signed-off-by: tinghsin <tinghsin@google.com>
    Signed-off-by: Will McVicker <willmcvicker@google.com>

--- drivers/pci/controller/dwc/pcie-exynos-common.h
+++ drivers/pci/controller/dwc/pcie-exynos-common.h
@@ -317,6 +317,7 @@ struct exynos_pcie {
 	struct pinctrl_state	*pin_state[MAX_PCIE_PIN_STATE];
 	struct pcie_eom_result **eom_result;
 	struct notifier_block	itmon_nb;
+	struct notifier_block   panic_nb;
 
 	struct gpio_desc *wlan_gpio;
 	struct gpio_desc *ssd_gpio;
