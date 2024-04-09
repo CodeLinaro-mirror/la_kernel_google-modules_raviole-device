9ac95fc14d181d3eacf1c11d2be932edb14614ec
Roy Luo <royluo@google.com>
2024-04-09 09:47:53 -0700

    tcpci_max777*9: update incompliance reason string for AICL case
    Use "input_power_limited" instead of "other".
    
    Bug: 308700954
    Test: verify HAL is getting the new string correctly
    Change-Id: I5c757236336e52af05c467a68bc8c9e2e49b08b0
    Signed-off-by: Roy Luo <royluo@google.com>
    Signed-off-by: Will McVicker <willmcvicker@google.com>

--- drivers/usb/typec/tcpm/google/tcpci_max77759.h
+++ drivers/usb/typec/tcpm/google/tcpci_max77759.h
@@ -234,11 +234,11 @@ void register_data_active_callback(void (*callback)(void *data_active_payload,
 						    enum typec_data_role role, bool active),
 				   void *data);
 
-/* AICL_OK is tracked with COMPLIANCE_WARNING_OTHER */
 #define COMPLIANCE_WARNING_OTHER 0
 #define COMPLIANCE_WARNING_DEBUG_ACCESSORY 1
 #define COMPLIANCE_WARNING_BC12 2
 #define COMPLIANCE_WARNING_MISSING_RP 3
+#define COMPLIANCE_WARNING_INPUT_POWER_LIMITED 4
 
 struct max77759_compliance_warnings {
 	struct max77759_plat *chip;
@@ -246,6 +246,7 @@ struct max77759_compliance_warnings {
 	bool debug_accessory;
 	bool bc12;
 	bool missing_rp;
+	bool input_power_limited;
 };
 
 ssize_t compliance_warnings_to_buffer(struct max77759_compliance_warnings *compliance_warnings,
