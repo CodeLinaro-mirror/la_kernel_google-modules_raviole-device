43edf902f3875da4673840c97ae32df6756965e8
Jack Diver <diverj@google.com>
2024-04-29 14:12:30 +0100

    pt: Disable IRQ when locking pt_internal_data.sl
    Bug: 337391620
    Test: Boot to home
    Change-Id: I291cb0a9bd341bd971d2851e159d08be5af038cf
    Signed-off-by: Jack Diver <diverj@google.com>
    (cherry picked from commit e9099db65673b55c45f758c58c923da3e926b96c)
    (cherry picked from commit cbddfd6cb7f7dcdd7e4b2f19e190cdd2ded8edb6)
    Signed-off-by: Will McVicker <willmcvicker@google.com>

--- drivers/soc/google/pt/pt.c
+++ drivers/soc/google/pt/pt.c
@@ -249,7 +249,7 @@ static int pt_resize_thread(void *data)
 	int id;
 
 	while (!kthread_should_stop()) {
-		spin_lock(&pt_internal_data.sl);
+		spin_lock_irq(&pt_internal_data.sl);
 
 		pts = pt_resize_list_next(&size);
 		if (pts != NULL) {
@@ -264,7 +264,7 @@ static int pt_resize_thread(void *data)
 				(int)size, pts->ptid);
 		}
 
-		spin_unlock(&pt_internal_data.sl);
+		spin_unlock_irq(&pt_internal_data.sl);
 
 		/* List was empty, wait to be notified by pt_resize_list_add */
 		if (pts == NULL) {
