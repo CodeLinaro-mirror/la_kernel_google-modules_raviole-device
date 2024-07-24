07f9f1c6c1521b83383f77f655edc556c9351dc3
André Draszik <draszik@google.com>
2024-07-24 10:16:45 +0100

    devfreq: google: remove second parameter of __assign_str() (v6.10 compatibility)
    Linux commit [1] has changed __assign_str() as the second argument is
    not required anymore due to other changes.
    
    Update this driver accordingly.
    
    Link: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=2c92ca849fcc [1]
          ("tracing/treewide: Remove second parameter of __assign_str()")
    Test: TH
    Bug: 354920620
    Change-Id: Ia515cb01f33ba9202e1981fa1cc3efec5e6ef014
    Signed-off-by: André Draszik <draszik@google.com>
    (cherry picked from commit 529c00944fa4a0b66f46bc5189ea61b9a4a3718f)
    Signed-off-by: Will McVicker <willmcvicker@google.com>

--- drivers/devfreq/google/governor_memlat_trace.h
+++ drivers/devfreq/google/governor_memlat_trace.h
@@ -31,7 +31,7 @@ TRACE_EVENT(memlat_dev_meas,
 	),
 
 	TP_fast_assign(
-		__assign_str(name, name);
+		__assign_str(name);
 		__entry->dev_id = dev_id;
 		__entry->inst = inst;
 		__entry->mem = mem;
@@ -67,7 +67,7 @@ TRACE_EVENT(memlat_dev_update,
 	),
 
 	TP_fast_assign(
-		__assign_str(name, name);
+		__assign_str(name);
 		__entry->dev_id = dev_id;
 		__entry->inst = inst;
 		__entry->mem = mem;
