41bf11b9f56b265100737f4ab9f379a891ee8e29
André Draszik <draszik@google.com>
2024-09-23 11:13:15 +0100

    thermal: samsung: gpu_cooling: tz field in struct thermal_instance is gone (v6.12 prep)
    Linux commit [1] has removed the tz field from struct thermal_instance.
    
    We can approximate this using thermal_zone_get_zone_by_name(), with
    subtle differences:
        1) the original code returns the last thermal zone registered with
           Linux in case the correct one wasn't found (by name) and uses
           that instead. The new code is more strict and only considers the
           matching zone.
        2) in case multiple zones with the same name had been registered,
           the old code considers the first one. The new code errors out
           (thermal_zone_get_zone_by_name() returns an error in this case)
        3) the old code only considers thermal zones actually bound to this
           specific cooling device cdev. The new code doesn't and looks at
           the global list of thermal zones instead.
    
    Despite those differences, my (non-exhaustive) tests suggest this
    replacement is viable, at least for now.
    
    Notes:
    1) I don't think the original behaviour from 1) makes sense.
    2) Point 2) should never happen.
    
    Link: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=e9654659fe3c [1]
          ("thermal: core: Drop tz field from struct thermal_instance")
    Test: TH
    Bug: 369062829
    Change-Id: I325b6671ea719eaec825de0eb2bd8609e369f506
    Signed-off-by: André Draszik <draszik@google.com>
    (cherry picked from commit b74fbcbc0bc7f83028c7d54c601a2b5168e49b43)
    Signed-off-by: Will McVicker <willmcvicker@google.com>

--- drivers/thermal/samsung/gpu_cooling.c
+++ drivers/thermal/samsung/gpu_cooling.c
@@ -845,26 +845,16 @@ static struct thermal_zone_device * parse_ect_cooling_level(
 	struct thermal_cooling_device *cdev, char *cooling_name)
 {
 	struct thermal_instance *instance;
-	struct thermal_zone_device *tz = NULL;
-	bool foundtz = false;
+	struct thermal_zone_device *tz;
 	void *thermal_block;
 	struct ect_ap_thermal_function *function;
 	int i, temperature;
 	unsigned int freq;
 	struct gpufreq_cooling_device *gpufreq_cdev = cdev->devdata;
 
-	mutex_lock(&cdev->lock);
-	list_for_each_entry(instance, &cdev->thermal_instances, cdev_node) {
-		tz = instance->tz;
-		if (!strncasecmp(cooling_name, tz->type, THERMAL_NAME_LENGTH)) {
-			foundtz = true;
-			break;
-		}
-	}
-	mutex_unlock(&cdev->lock);
-
-	if (!foundtz)
-		goto skip_ect;
+	tz = thermal_zone_get_zone_by_name(cooling_name);
+	if (IS_ERR(tz))
+		return NULL;
 
 	thermal_block = ect_get_block(BLOCK_AP_THERMAL);
 	if (!thermal_block)
