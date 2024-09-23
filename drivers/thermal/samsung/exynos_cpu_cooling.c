22efb10af252bf27e9c0011166ac7357b72844ab
André Draszik <draszik@google.com>
2024-09-23 15:40:33 +0100

    thermal: samsung: cpu_cooling: get_thermal_instance() is gone (v6.12 prep)
    Linux commit [1] has removed get_thermal_instance().
    
    parse_ect_cooling_level() appears unused, as it returns early in all
    cases of 'cooling_name':
        * for BIG and MID, ect_ap_thermal_get_function() always returns
          NULL
        * for LITTLE, get_thermal_instance() always returns NULL
    
    We can therefore simply leave the assignment of cpufreq_cdev->tzd and
    drop the remaining code.
    
    This drops the use of get_thermal_instance() and the result should be
    the same, as parse_ect_cooling_level() effectively does nothing else.
    
    Link: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=96d819908de0 [1]
          ("thermal: helpers: Drop get_thermal_instance()")
    Test: TH
    Bug: 369110000
    Change-Id: I7053a856cc5fbbfb1b459cb0a42d191b50c3bee3
    Signed-off-by: André Draszik <draszik@google.com>
    (cherry picked from commit 54c4f49d606e601e956a40dabc48657ad80bb55a)
    Signed-off-by: Will McVicker <willmcvicker@google.com>

--- drivers/thermal/samsung/exynos_cpu_cooling.c
+++ drivers/thermal/samsung/exynos_cpu_cooling.c
@@ -852,57 +852,6 @@ static unsigned int find_next_max(struct cpufreq_frequency_table *table,
 	return max;
 }
 
-static struct thermal_zone_device *parse_ect_cooling_level(
-	struct thermal_cooling_device *cdev, char *cooling_name)
-{
-	struct thermal_instance *instance;
-	struct thermal_zone_device *tz;
-	void *thermal_block;
-	struct ect_ap_thermal_function *function;
-	int i, temperature;
-	unsigned int freq;
-
-	tz = thermal_zone_get_zone_by_name(cooling_name);
-	if (IS_ERR(tz))
-		return NULL;
-
-	thermal_block = ect_get_block(BLOCK_AP_THERMAL);
-	if (!thermal_block)
-		goto skip_ect;
-
-	function = ect_ap_thermal_get_function(thermal_block, cooling_name);
-	if (!function)
-		goto skip_ect;
-
-	for (i = 0; i < function->num_of_range; ++i) {
-		struct exynos_cpu_cooling_device *cpufreq_cdev = cdev->devdata;
-		unsigned long max_level = 0;
-		int level;
-
-		temperature = function->range_list[i].lower_bound_temperature;
-		freq = function->range_list[i].max_frequency;
-
-		instance = get_thermal_instance(tz, cdev, i);
-		if (!instance) {
-			pr_err("%s: (%s, %d)instance isn't valid\n", __func__, cooling_name, i);
-			goto skip_ect;
-		}
-
-		cdev->ops->get_max_state(cdev, &max_level);
-		level = get_level(cpufreq_cdev, freq);
-
-		if (level == THERMAL_CSTATE_INVALID)
-			level = max_level;
-
-		instance->upper = level;
-
-		pr_info("Parsed From ECT : %s: [%d] Temperature : %d, frequency : %u, level: %d\n",
-			cooling_name, i, temperature, freq, level);
-	}
-skip_ect:
-	return tz;
-}
-
 ssize_t
 state2power_table_show(struct device *dev, struct device_attribute *attr, char *buf)
 {
@@ -1136,7 +1085,9 @@ __exynos_cpu_cooling_register(struct device_node *np,
 	}
 
 	/* This needs to be done before thermal_of_cooling_device_register ... */
-	cpufreq_cdev->tzd = parse_ect_cooling_level(cdev, cooling_name);
+	cpufreq_cdev->tzd = thermal_zone_get_zone_by_name(cooling_name);
+	if (IS_ERR(cpufreq_cdev->tzd))
+		cpufreq_cdev->tzd = NULL;
 
 	mutex_lock(&cooling_list_lock);
 	list_add(&cpufreq_cdev->node, &cpufreq_cdev_list);
