5b0d387f196bfd2b7f02123124c023e8e5d2b2ff
Aleksei Vetrov <vvvvvv@google.com>
2024-10-02 14:27:28 +0000

    usb: phy: phy-exynos-usb3p1: fix enum type mismatch
    Updated Clang compiler found enum type mismatch: cr_op has type
    exynos_usbcon_ssp_cr and it is being compared with USBCON_CR_READ and
    USBCON_CR_WRITE from exynos_usbcon_cr. Replace them with the same values
    from the matching enum type.
    
    Bug: 354745907
    Test: TH
    Change-Id: I783f75b0426945b6bda46d0a9e4b7507e728930b
    Signed-off-by: Aleksei Vetrov <vvvvvv@google.com>
    Signed-off-by: Will McVicker <willmcvicker@google.com>

--- drivers/phy/samsung/phy-exynos-usb3p1.c
+++ drivers/phy/samsung/phy-exynos-usb3p1.c
@@ -780,7 +780,7 @@ static u16 phy_exynos_usb_v3p1_ssp_cr_access(struct exynos_usbphy_info *info,
 			    (G2PHY_CRPARCON0_CR_PARA_ADDR(addr) |
 			     (G2PHY_CRPARCON0_CR_PARA_CLK));
 
-			if (cr_op == USBCON_CR_WRITE) {
+			if (cr_op == USBCON_CR_SSP_WRITE) {
 				g2phy_crparcon2 =
 				    readl(base + EXYNOS_USBCON_G2PHY_CRPARCON2);
 				g2phy_crparcon2 |=
@@ -794,7 +794,7 @@ static u16 phy_exynos_usb_v3p1_ssp_cr_access(struct exynos_usbphy_info *info,
 				       base + EXYNOS_USBCON_G2PHY_CRPARCON2);
 
 			} else {
-				/* cr_op==USBCON_CR_READ */
+				/* cr_op==USBCON_CR_SSP_READ */
 				g2phy_crparcon1 =
 				    readl(base + EXYNOS_USBCON_G2PHY_CRPARCON1);
 				g2phy_crparcon1 |=
@@ -812,10 +812,10 @@ static u16 phy_exynos_usb_v3p1_ssp_cr_access(struct exynos_usbphy_info *info,
 			    ~(0x1 << phy_exynos_usb_v3p1_ssp_cr_clk_down(info));
 
 		} else if (loop_cnt == 2) {
-			if (cr_op == USBCON_CR_WRITE) {
+			if (cr_op == USBCON_CR_SSP_WRITE) {
 				/* Do nothing */
 			} else {
-				/* cr_op==USBCON_CR_READ */
+				/* cr_op==USBCON_CR_SSP_READ */
 				g2phy_crparcon1 =
 				    readl(base + EXYNOS_USBCON_G2PHY_CRPARCON1);
 				g2phy_crparcon1 &=
