9b9e4e1ea8b525236901b17397f5ae6cdec472d7
André Draszik <draszik@google.com>
2024-10-08 15:59:24 +0100

    Input: s2mpg12-key: plug memleak: case 2.1.2: of_find_node_by_name() (v6.12 prep)
    Case 2.1.2 from below bug:
    
        The struct device_node returned from of_find_node_by_name() needs
        to be released using of_node_put() when done with it to avoid a
        memleak.
    
    Do so using a cleanup handler that will do so on function exit for
    normal and error cases without having to adjust all code paths.
    
    This commit plugs memleaks in both normal and error code paths.
    
    Test: TH
    Bug: 370679843
    Change-Id: I1b93a94ab522025e5c45116ab9a4d6b48169a994
    Signed-off-by: André Draszik <draszik@google.com>
    Signed-off-by: Will McVicker <willmcvicker@google.com>

--- drivers/input/keyboard/s2mpg12-key.c
+++ drivers/input/keyboard/s2mpg12-key.c
@@ -10,6 +10,7 @@
  * published by the Free Software Foundation.
  */
 
+#include <linux/cleanup.h>
 #include <linux/module.h>
 #include <linux/init.h>
 #include <linux/interrupt.h>
@@ -183,7 +184,8 @@ power_keys_get_devtree_pdata(struct s2mpg12_dev *iodev)
 {
 	#define S2MPG12_SUPPORT_KEY_NUM	(1)
 	struct device *dev = iodev->dev;
-	struct device_node *mfd_np, *key_np, *pp;
+	struct device_node *mfd_np, *pp;
+	struct device_node *key_np __free(device_node) = NULL;
 	struct power_keys_platform_data *pdata;
 	struct power_keys_button *button;
 	int error, nbuttons, i;
