af11d82e20c88ec984d7493025e8fcb130dfad9b
André Draszik <draszik@google.com>
2024-10-09 09:30:43 +0100

    mfd: slg51002: use correct value for gpiod_direction_output(_raw) (v6.12 prep)
    Linux commit [1] has removed GPIOF_DIR_OUT and GPIOF_DIR_IN.
    
    This causes this OOT Pixel driver to fail compilation.
    
    It also highlights incorrect use of this flag while calling
    gpiod_direction_output(). There is no symbolic value to be used as
    argument, drivers are meant to pass 0 or 1, as appropriate, see
    Documentation/driver-api/gpio/intro.rst in the Linux source tree.
    
    To fix this obvious bug and in preparation of that kernel change,
    switch to using the correct value.
    
    Link: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=8c045ca534d0 [1]
          ("gpiolib: legacy: Kill GPIOF_DIR_* definitions")
    Test: TH, compiles locally
    Bug: 369317190
    Change-Id: I3080068b5324c292958b64ad5ff011ca58a8b662
    Signed-off-by: André Draszik <draszik@google.com>
    Signed-off-by: Will McVicker <willmcvicker@google.com>

--- drivers/mfd/slg51002-core.c
+++ drivers/mfd/slg51002-core.c
@@ -773,24 +773,24 @@ static void slg51002_i2c_remove(struct i2c_client *client)
 
 	if (gpio_is_valid(slg51002->chip_pu_pin)) {
 		desc = gpio_to_desc(slg51002->chip_pu_pin);
-		ret |= gpiod_direction_output_raw(desc, GPIOF_INIT_LOW);
+		ret |= gpiod_direction_output_raw(desc, 0);
 		usleep_range(1000, 1020);
 	}
 	if (gpio_is_valid(slg51002->chip_cs_pin)) {
 		desc = gpio_to_desc(slg51002->chip_cs_pin);
-		ret |= gpiod_direction_output_raw(desc, GPIOF_INIT_LOW);
+		ret |= gpiod_direction_output_raw(desc, 0);
 		/* Put SLG51002 back to Reset state */
 		usleep_range(SLEEP_10000_USEC,
 				SLEEP_10000_USEC + SLEEP_RANGE_USEC);
 	}
 	if (gpio_is_valid(slg51002->chip_buck_pin)) {
 		desc = gpio_to_desc(slg51002->chip_buck_pin);
-		ret |= gpiod_direction_output_raw(desc, GPIOF_INIT_LOW);
+		ret |= gpiod_direction_output_raw(desc, 0);
 		usleep_range(1000, 1020);
 	}
 	if (gpio_is_valid(slg51002->chip_bb_pin)) {
 		desc = gpio_to_desc(slg51002->chip_bb_pin);
-		ret |= gpiod_direction_output_raw(desc, GPIOF_INIT_LOW);
+		ret |= gpiod_direction_output_raw(desc, 0);
 		usleep_range(1000, 1020);
 	}
 
