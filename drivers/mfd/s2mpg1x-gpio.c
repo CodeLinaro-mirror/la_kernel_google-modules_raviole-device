3dd4ad6ad3283a964701ebdd709ba562a8a08bd4
André Draszik <draszik@google.com>
2024-10-09 09:03:14 +0100

    mfd: s2mpg1x-gpio: plug memleak: case 2.1.2: of_find_node_by_name() (v6.12 prep)
    Case 2.1.2 from below bug:
    
        The struct device_node returned from of_find_node_by_name() needs
        to be released using of_node_put() when done with it to avoid a
        memleak.
    
    Do so using of_node_put().
    
    This commit plugs memleaks in error code paths.
    
    Test: TH
    Bug: 370679843
    Change-Id: I366e96bf2faa4da6b2c20045aed8560c7e45e187
    Signed-off-by: André Draszik <draszik@google.com>
    Signed-off-by: Will McVicker <willmcvicker@google.com>

--- drivers/mfd/s2mpg1x-gpio.c
+++ drivers/mfd/s2mpg1x-gpio.c
@@ -464,6 +464,7 @@ static int s2mpg1x_gpio_probe(struct platform_device *pdev)
 	if (of_property_read_u32(dp, "ngpios", &ngpios)) {
 		dev_err(&pdev->dev, "Failed to get ngpios from %s DT node\n",
 			pdev->name);
+		of_node_put(dp);
 		return -EINVAL;
 	}
 	s2mpg1x_gpio->gc.ngpio = ngpios;
