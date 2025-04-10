77f64db7289292749fa4e68c8911193b5424ff58
Tudor Ambarus <tudordana@google.com>
2025-04-10 11:34:11 +0000

    media: mfc_dec_v4l2: remove g/s_ctrl callbacks (v6.15 preparation)
    Upstream commits [1] and [2] removed the vidioc_g/s_ctrl callbacks and
    caused the OOT pixel mfc driver to fail compilation.
    
    The ioctl helpers can emulate g/s_ctrl with g/s_ext_ctrl. But the driver
    does not implement s_ext_ctrl, so s_ctrl functionality is removed
    without replacement.
    
    Bug: 409727213
    Test: TH
    Link: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=70307f28ee61 [1]
    Link: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=0d75129312ff [2]
    Change-Id: I0cbe33d82dc1fe8aeab18169cc9abadaf17a0079
    Signed-off-by: Tudor Ambarus <tudordana@google.com>

--- drivers/media/platform/exynos/mfc/mfc_dec_v4l2.c
+++ drivers/media/platform/exynos/mfc/mfc_dec_v4l2.c
@@ -76,8 +76,8 @@ static struct v4l2_queryctrl *__mfc_dec_get_ctrl(int id)
 	return NULL;
 }
 
-/* Check whether a ctrl value if correct */
-static int __mfc_dec_check_ctrl_val(struct mfc_ctx *ctx, struct v4l2_control *ctrl)
+/* TODO: b/409727213. Check whether a ctrl value if correct */
+static int __maybe_unused __mfc_dec_check_ctrl_val(struct mfc_ctx *ctx, struct v4l2_control *ctrl)
 {
 	struct v4l2_queryctrl *c;
 
@@ -1351,23 +1351,9 @@ static int __mfc_dec_get_ctrl_val(struct mfc_ctx *ctx, struct v4l2_control *ctrl
 	return 0;
 }
 
-/* Get a ctrl */
-static int mfc_dec_g_ctrl(struct file *file, void *priv,
-			struct v4l2_control *ctrl)
-{
-	struct mfc_ctx *ctx = fh_to_mfc_ctx(file->private_data);
-	int ret = 0;
-
-	mfc_debug_enter();
-	ret = __mfc_dec_get_ctrl_val(ctx, ctrl);
-	mfc_debug_leave();
-
-	return ret;
-}
-
-/* Set a ctrl */
-static int mfc_dec_s_ctrl(struct file *file, void *priv,
-			 struct v4l2_control *ctrl)
+/* TODO: b/409727213. Set a ctrl */
+static int __maybe_unused mfc_dec_s_ctrl(struct file *file, void *priv,
+					 struct v4l2_control *ctrl)
 {
 	struct mfc_ctx *ctx = fh_to_mfc_ctx(file->private_data);
 	struct mfc_dec *dec = ctx->dec_priv;
@@ -1624,8 +1610,8 @@ static const struct v4l2_ioctl_ops mfc_dec_ioctl_ops = {
 	.vidioc_streamon		= mfc_dec_streamon,
 	.vidioc_streamoff		= mfc_dec_streamoff,
 	.vidioc_queryctrl		= mfc_dec_queryctrl,
-	.vidioc_g_ctrl			= mfc_dec_g_ctrl,
-	.vidioc_s_ctrl			= mfc_dec_s_ctrl,
+	//TODO: b/409727213
+	//.vidioc_s_ctrl		= mfc_dec_s_ctrl,
 	.vidioc_g_selection		= mfc_dec_g_selection,
 	.vidioc_g_ext_ctrls		= mfc_dec_g_ext_ctrls,
 };
