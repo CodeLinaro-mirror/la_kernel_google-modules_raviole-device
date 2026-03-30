3173040c5b2b8b224af3e3dce679cfaad654a741
Mostafa Saleh <smostafa@google.com>
2026-03-30 09:53:39 +0000

    slider: Dummy kvm IOMMU module
    This prevents clearing pvm firmware due to no registered IOMMU,
    and therefore allow pKVM tests to pass.
    
    Bug: 489096478
    Bug: 489788025
    Change-Id: Iedbd86f7c12f85c145196f04b7b293a9333b819e
    Signed-off-by: Keir Fraser <keirf@google.com>
    Signed-off-by: Mostafa Saleh <smostafa@google.com>

--- drivers/iommu/kvm-dummy-iommu.c
+++ drivers/iommu/kvm-dummy-iommu.c
@@ -18,30 +18,13 @@ static int kvm_dummy_iommu_init(void)
 	return 0;
 }
 
-static void kvm_dummy_iommu_remove(void)
-{
-}
-
-static int kvm_dummy_iommu_id(struct device *dev, u32 id, pkvm_handle_t *out_iommu, u32 *out_sid)
-{
-	return 0;
-}
-
-static pkvm_handle_t kvm_dummy_iommu_id_by_of(struct device_node *np)
-{
-	return 0;
-}
-
 struct kvm_iommu_driver kvm_smmu_v3_ops = {
 	.init_driver = kvm_dummy_iommu_init,
-	.remove_driver = kvm_dummy_iommu_remove,
-	.get_device_iommu_id = kvm_dummy_iommu_id,
-	.get_iommu_id_by_of = kvm_dummy_iommu_id_by_of,
 };
 
 static int kvm_dummy_iommu_register(void)
 {
-	return kvm_iommu_register_driver(&kvm_smmu_v3_ops);
+	return kvm_iommu_register_driver(&kvm_smmu_v3_ops, 0);
 }
 
 /*
